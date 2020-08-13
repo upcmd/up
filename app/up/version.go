3fe90be276919697bf69fe6d3f1e2037578df1e2
Stephen Cheng <stephencheng@users.noreply.github.com>
2020-08-14 09:56:24 +1000

    Update version.go

--- app/up/version.go
+++ app/up/version.go
@@ -3,8 +3,8 @@ package main
 var version_info = `
 version_info:
   Type: Unofficial build
-	Note: |
-		An official build should provide meta data of version and build date
-		Refer to: https://github.com/upcmd/up/releases for downloading
-		For redistributing, please use up.yml build process to package this version_info properly
+  Note: |
+    An official build should provide meta data of version and build date
+    Refer to: https://github.com/upcmd/up/releases for downloading
+    For redistributing, please use up.yml build process to package this version_info properly
 `
